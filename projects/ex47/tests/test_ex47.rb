fdf8cb57a3d92e1ad57c86f5738ef5667a4023ac
Stacey Lin <twstacey@gmail.com>
2015-10-05 17:49:26 -0700

    Added test function for mini-version of game from Exercise 46

--- projects/ex47/tests/test_ex47.rb
+++ projects/ex47/tests/test_ex47.rb
@@ -7,8 +7,8 @@ class TestName < Test::Unit::TestCase
     gold = Room.new("GoldRoom",
       """This room has gold in it you can grab. There's alias_method 
       door to the north.""")
-    assert_equal("Gold Room", gold.name)
-    assert_equal({}, gold.paths)
+    assert_equal(gold.name, "GoldRoom")
+    assert_equal(gold.paths, {})
   end
 
   def test_room_paths()
@@ -37,6 +37,38 @@ class TestName < Test::Unit::TestCase
   end
 
   def test_game()
+    basement = Room.new("Basement", "You can go to Dress.")
+    dress = Room.new("Dress", "You can go to Necklace, Dress, or Lose.")
+    necklace = Room.new("Necklace", "You can go to Zombie or Lose.")
+    zombie = Room.new("Zombie", "You can go to Merman or Lose.")
+    merman = Room.new("Merman", "You can go to Lose or Finish")
+    lose = Room.new("Lose", "You have lost the game. Nowhere to go.")
+    finish = Room.new("Finish", "You have won the game. Nowhere to go.")
+
+    # from basement, can only go to dress
+    basement.add_paths({'dress' => dress})
+    # from dress, can go to necklace, dress, or lose
+    dress.add_paths({'necklace' => necklace, 'dress' => dress, 'lose' => lose})
+    # from necklace, can go to zombie, or lose
+    necklace.add_paths({'zombie' => zombie, 'lose' => lose})
+    # from zombie, can go to merman or lose
+    zombie.add_paths({'merman' => merman, 'lose' => lose})
+    # from merman, can go to lose or finish
+    merman.add_paths({'lose' => lose, 'finish' => finish})
+    ## lose and finish do not have paths to any other rooms
+
+    # test that basement goes to dress
+    assert_equal(basement.go('dress'), dress)
+    # test that dress goes to dress
+    assert_equal(dress.go('dress'), dress)
+    # test that dress goes to lose
+    assert_equal(dress.go('lose'), lose)
+    # test that dress goes to necklace, goes to zombie, goes to lose
+    assert_equal(dress.go('necklace').go('zombie').go('lose'), lose)
+    # test that zombie goes to merman, goes to lose
+    assert_equal(zombie.go('merman').go('lose'), lose)
+    # test that merman goes to finish
+    assert_equal(merman.go('finish'), finish)
   end
-  
+
 end
