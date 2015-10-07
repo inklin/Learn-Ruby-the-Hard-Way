71a03ad8a5bc0cb2a409b709eb26c07296663279
Stacey Lin <twstacey@gmail.com>
2015-10-07 01:28:13 -0700

    Added words to lexicon and test methods

--- ex48/lexicon.rb
+++ ex48/lexicon.rb
@@ -1,43 +1,35 @@
 class Lexicon
   @@dictionary = {
     'direction' => ['north', 'south', 'east', 'west'],
-    'verb' => ['go', 'eat', 'kill'],
-    'stop' => ['the', 'in', 'of'],
-    'noun' => ['bear', 'princess']
+    'verb' => ['go', 'eat', 'kill', 'jump', 'run',
+      'walk'],
+    'stop' => ['the', 'in', 'of', 'to', 'a', 'is'],
+    'noun' => ['bear', 'princess'],
+    'name' => ['snoopy', 'linus', 'lucy']
   }
 
   def self.scan(text)
     text_elements = text.split(' ')
     result = []
 
-    # go through each text element
     text_elements.each do |text_element|
       categorized = []
 
-      # first we check if the word is a number, 
-      # we do this first so we don't have to go through the dictionary
-      # if it's a number
-      number = Lexicon.convert_number(text_element)
+      number = Lexicon.integer?(text_element)
       if (number) 
         categorized = ['number', number]
         result.push(categorized)
         next
       end
 
-      # go through the dictionary
       @@dictionary.each do |type, list|
-        # see if the element is in the word list
-        if list.include?(text_element)
-          # if it is, add the categorized word to the result
-          # and you don't have to look at the other word lists
-          # so break out of the dictionary loop
+        if list.include?(text_element.downcase)
           categorized = [type, text_element]
           result.push(categorized)
           break
         end
       end
 
-      # if the word is not categorized, add it as an error
       if (categorized.empty?)
         categorized = ['error', text_element]
         result.push(categorized)
@@ -48,11 +40,15 @@ class Lexicon
     return result
   end
 
-  def self.convert_number(object)
+  def self.integer?(object)
+    # remove any trailing zeros so the number
+    # does not get interpreted as hex
+    object.sub!(/^0+/, "")
+
     begin
       return Integer(object)
     rescue
-      return nil
+      return false
     end
   end
 
--- ex48/tests/test_ex48.rb
+++ ex48/tests/test_ex48.rb
@@ -5,10 +5,11 @@ class TestEx48 < Test::Unit::TestCase
 
   def test_directions()
     assert_equal([['direction', 'north']], Lexicon.scan("north"))
-    result = Lexicon.scan("north south east")
+    result = Lexicon.scan("north south east west")
 
     assert_equal([['direction', 'north'],
-      ['direction', 'south'], ['direction', 'east']], result)
+      ['direction', 'south'], ['direction', 'east'],
+      ['direction', 'west']], result)
   end
 
   def test_verbs()
@@ -17,6 +18,10 @@ class TestEx48 < Test::Unit::TestCase
 
     assert_equal([['verb', 'go'],
       ['verb', 'kill'], ['verb', 'eat']], result)
+
+    result = Lexicon.scan("jump walk run")
+    assert_equal([['verb', 'jump'],
+      ['verb', 'walk'], ['verb', 'run']], result)
   end
 
   def test_stops()
@@ -24,6 +29,10 @@ class TestEx48 < Test::Unit::TestCase
     result = Lexicon.scan("the in of")
     assert_equal([['stop', 'the'],
       ['stop', 'in'], ['stop', 'of']], result)
+
+    result = Lexicon.scan("to is a")
+    assert_equal([['stop', 'to'],
+      ['stop', 'is'], ['stop', 'a']], result)
   end
 
   def test_nouns()
@@ -34,14 +43,33 @@ class TestEx48 < Test::Unit::TestCase
 
   def test_numbers()
     assert_equal([['number', 1234]], Lexicon.scan("1234"))
-    result = Lexicon.scan("3 91234")
-    assert_equal([['number', 3], ['number', 91234]], result)
+    result = Lexicon.scan("3 91234 034")
+    assert_equal([['number', 3], ['number', 91234],
+      ['number', 34]], result)
+  end
+
+  def test_names()
+    assert_equal([['name', 'Snoopy'], ['name', 'linus'],
+      ['name', 'Lucy']], Lexicon.scan("Snoopy linus Lucy"))
   end
 
   def test_errors()
     assert_equal([['error', 'ASDFADFASDF']], Lexicon.scan("ASDFADFASDF"))
     result = Lexicon.scan("bear IAS princess")
     assert_equal([['noun', 'bear'], ['error', 'IAS'], ['noun', 'princess']], result)
+  end
+
+  def test_capitalization()
+    assert_equal([['noun', 'Bear']], Lexicon.scan("Bear"))
+    result = Lexicon.scan("Bear iS a PRINCESS")
+    assert_equal([['noun', 'Bear'], ['stop', 'iS'],
+      ['stop', 'a'], ['noun', 'PRINCESS']], result)
+  end
+
+  def test_combination()
+    result = Lexicon.scan("the bear go north 88 eat")
+    assert_equal([['stop', 'the'],['noun', 'bear'], ['verb', 'go'],
+      ['direction', 'north'], ['number', 88], ['verb', 'eat']], result)
   end
 
 end
