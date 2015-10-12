6148745b3ebec52c957f6d0666588949fc1c4442
Stacey Lin <twstacey@gmail.com>
2015-10-12 16:27:05 -0700

    Added Image Upload

--- projects/gothonweb/bin/app.rb
+++ projects/gothonweb/bin/app.rb
@@ -5,6 +5,11 @@ set :static, true
 set :public_folder, "static"
 set :views, "views"
 
+## Get images and display
+def load_pictures
+  return Dir.glob("static/images/*.{jpg,jpeg}")
+end
+
 get '/' do
   return "Hello World"
 end
@@ -17,6 +22,27 @@ post '/hello/' do
   greeting = params[:greeting] || "Hello There"
   sender = params[:sender] || "Nobody"
   recipient = params[:recipient] || "You"
+  puts recipient
 
   erb :index, :locals => {'greeting' => greeting, 'sender' => sender, 'recipient' => recipient}
+end
+
+get '/upload/' do
+  @pictures = load_pictures
+  erb :upload_form
+end
+
+## Image Upload Page
+post '/upload/' do
+  # log what the file object looks like
+  uploaded_file = params[:myfile]
+  puts "The file object is #{uploaded_file}"
+  # create a new file in public/images/filename 
+  target = open('./static/images/' + uploaded_file[:filename], "w")
+  # now we have to write the file to our empty file
+  target.write(uploaded_file[:tempfile].read)
+  target.close()
+  
+  @pictures = load_pictures
+  erb :upload_form
 end
