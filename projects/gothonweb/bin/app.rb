1d5e8da03301b9303c58b6860abd7371c0c38705
Stacey Lin <twstacey@gmail.com>
2015-10-11 19:06:17 -0700

    Improved form HTML layout

--- projects/gothonweb/bin/app.rb
+++ projects/gothonweb/bin/app.rb
@@ -5,39 +5,18 @@ set :static, true
 set :public_folder, "static"
 set :views, "views"
 
-get '/post/' do 
-  erb :greeting, :layout => :post
-end
-
-# redirect ohaiyo to hello
-get '/ohaiyo/' do 
-  redirect to ('/hello/')
-end
-
-# create an html page
-get '/mypage/' do
-  person = params['name'] || "Person"
-  erb :mypage, :locals => {'user' => person}
+get '/' do
+  return "Hello World"
 end
 
 get '/hello/' do
-  greeting = params[:greeting] || "Hello there"
-  erb :index, :locals => {'greeting' => greeting}
+  erb :hello_form
 end
 
-get '/greet/:name' do
-  return "Say hello to #{params['name']}!"
-end
+post '/hello/' do
+  greeting = params[:greeting] || "Hello There"
+  sender = params[:sender] || "Nobody"
+  recipient = params[:recipient] || "You"
 
-get '/say/*/to/*' do
-  return "So, you want to say #{params['splat'][0]} to #{params['splat'][1]}!"
-end
-
-get '/time/' do
-  erb :mypage
-end
-
-# literal template
-get '/literal/' do
-  erb '%div.title This will be a string'
+  erb :index, :locals => {'greeting' => greeting, 'sender' => sender, 'recipient' => recipient}
 end
--- projects/gothonweb/tests/test_gothonweb.rb
+++ projects/gothonweb/tests/test_gothonweb.rb
@@ -1,10 +1,29 @@
-require "./lib/NAME.rb"
-require "test/unit"
+require './bin/app.rb'
+require 'test/unit'
+require 'rack/test'
+class MyAppTest < Test::Unit::TestCase
+  include Rack::Test::Methods
 
-class TestName < Test::Unit::TestCase
+  def app
+    Sinatra::Application
+  end
 
-  def test_sample
-    assert_equal(4, 2+2)
+  def test_my_default
+    get '/'
+    assert last_response.ok?
+    assert_equal 'Hello World', last_response.body
+  end
+
+  def test_hello_form
+    get '/hello/'
+    assert last_response.ok?
+    assert last_response.body.include?('A Greeting')
+  end
+
+  def test_hello_form_post
+    post '/hello/', params={:name => 'Stacey', :greeting => 'Hi'}
+    assert last_response.ok?
+    assert last_response.body.include?('I just wanted to say')
   end
 
 end
